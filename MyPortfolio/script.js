aa1ea175a76d0fb7b9127ea49ad0a7b82648a348
Ameysalaskar <148460530+Ameysalaskar@users.noreply.github.com>
2024-12-14 11:13:10 +0530

    Add files via upload

---
+++ MyPortfolio/script.js
@@ -0,0 +1,12 @@
+const navDialog = document.getElementById('nav-dialog');
+
+function handleMenu(){
+    navDialog.classList.toggle('hidden')
+}
+
+// document.addEventListener("mousemove", function(dets){
+//     gsap.to("#cursor",{
+//         left: dets.x,
+//         top: dets.y
+//     })
+// })
